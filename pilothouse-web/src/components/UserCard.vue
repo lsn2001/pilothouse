6f43ae67b2e0e765713a56faf80e2eb8e218221a
lsn2001 <642634743@qq.com>
2022-10-13 01:27:54 +0800

    feat:对多处重复的代码进行优化，对公共部分进行抽离

--- pilothouse-web/src/components/UserCard.vue
+++ pilothouse-web/src/components/UserCard.vue
@@ -1,166 +1,86 @@
 <script setup lang="ts">
-
-
-const cardList: any = [
-    {
-        itemImage: './../imgs/1.png',
-        name: 'Guillaume Chau',
-        info: 'Vue / Histoire',
-        intro: 'Histoire Deep Dive',
-        icon: './../imgs/icon1.svg',
-        bgcolor: 'bg-gradient-to-b from-green-300 via-green-400 to-green-500'
+const props:any = defineProps({
+    avatar: {
+        type: String,
+        default: '',
+        required: true
     },
-    {
-        itemImage: './../imgs/2.png',
-        name: 'Vojtech Miksu',
-        info: 'Uber / Ladle',
-        intro: 'introducing Ladle:Develop and test your React stories faster',
-        icon: './../imgs/icon2.svg',
-        bgcolor: 'bg-gradient-to-b from-blue-300 via-blue-400 to-blue-500'
+    name: {
+        type: String,
+        default: '',
+        required: true
     },
-    {
-        itemImage: './../imgs/3.png',
-        name: 'lan VanSchooten',
-        info: 'Defined Networking / Storybook',
-        intro: 'Developing,Documenting,and Testing your Vite app with Stroybook',
-        icon: './../imgs/icon3.svg',
-        bgcolor: 'bg-gradient-to-b from-red-300 via-red-400 to-red-500'
+    bgcolor: {
+        type: String,
+        default: 'white',
+        required: true
+    },
+    arr: {
+        type: String,
+        default: 'all',
     }
-]
+})
 
+const Arr:Array<string> = ['i-carbon:share','i-carbon:logo-twitter','i-carbon:logo-github']
+const LogoType = computed(() => {
+    let arr1: Array<number> = [...props.arr]
+    let arr2:Array<string> = []
+    for (let i = 0; i < arr1.length; i++){
+        if (arr1[i] == 1) {
+            arr2.push(Arr[0])
+        }
+        if (arr1[i] == 2) {
+            arr2.push(Arr[1])
+        }
+        if (arr1[i] == 3) {
+            arr2.push(Arr[2])
+        }
+        
+    }
+    return arr2
+})
+
+
+    
 </script>
 <template>
-    <div class="UserCard bg-black w-1668px">
-        <div class='container'>
-            <div class='card-list w-1650px flex justify-center items-center'>
-                <div class="list m-20px flex justify-center items-center">
-                    <div class="item opacity-90 hover:scale-110 hover:opacity-100 transition duration-150 mx-20px w-380px h-470px text-center rounded-20px" :class="item.bgcolor" v-for="(item,j) in cardList" :key="j">
-                        <div class="item-logo w-full h-40px flex justify-between items-center">
-                            <div class="icon">
-                                <img class='info-icon w-30px h-30px ml-20px mt-20px' :src="item.icon" alt="">
-                            </div>
-                            <div class="share-icon h-20px w-130px mr-10px mt-20px">
-                                <a class="share opacity-30 hover:opacity-100 hover:scale-120 transition duration-150 mr-4 inline-block w-25px h-25px opacity-50 bg-no-repeat bg-contain" style="background-image: url('https://api.iconify.design/carbon:share.svg');" href="javascript:;"></a>
-                                <a class="bluebird opacity-30 hover:opacity-100 hover:scale-120 transition duration-150 mr-4 inline-block w-25px h-25px opacity-50 bg-no-repeat bg-contain" style="background-image: url('./../imgs/bluebird.svg');" href="javascript:;"></a>
-                                <a class="github opacity-30 hover:opacity-100 hover:scale-120 transition duration-150 mr-4 inline-block w-25px h-25px opacity-50 bg-no-repeat bg-contain" style="background-image: url('./../imgs/github.svg');" href="javascript:;"></a>
-                            </div>
-                        </div>
-                        <div class="item-img my-30px">
-                            <el-avatar shape="square" :size="164" :src="item.itemImage"></el-avatar>
-                        </div>
-                        <div class="item-info">
-                            <span block text-2xl>{{item.name}}</span>
-                            <span block>{{item.info}}</span>
-                        </div>
-                        <div class="item-intro h-80px mt-30px mb-10px mx-10px flex justify-center items-center border-solid border black-200">
-                            <span class="intro inline-block mx-10px">{{item.intro}}</span>
+    <div bg-black w-1857px flex justify-center items-center>
+        <div mx-20px  flex justify-center items-center>
+            <div mr-10px flex justify-center items-center>
+                <div opacity-90 hover:scale-110 hover:opacity-100 transition duration-150 mx-20px w-380px h-470px
+                    text-center rounded-20px :class="bgcolor" >
+                    <div w-full h-40px flex justify-between items-center>
+                            <slot name="icon"></slot>
+                            <!-- <img w-30px h-30px ml-20px mt-20px :src="$props.icon" alt=""> -->
+                        
+                        <div h-20px w-130px mr-10px mt-20px >
+                            <a opacity-30 hover:opacity-100 hover:scale-120 transition duration-150 mr-4 inline-block
+                                w-25px h-25px opacity-50 bg-no-repeat bg-contain v-for="(item,j) in LogoType" :key="j" :class=item href="javascript:;"></a>
+                            <!-- <a opacity-30 hover:opacity-100 hover:scale-120 transition duration-150 mr-4 inline-block
+                                w-25px h-25px opacity-50 bg-no-repeat bg-contain i-carbon:logo-twitter
+                                href="javascript:;"></a>
+                            <a opacity-30 hover:opacity-100 hover:scale-120 transition duration-150 mr-4 inline-block
+                                w-25px h-25px opacity-50 bg-no-repeat bg-contain i-carbon:logo-github
+                                href="javascript:;"></a> -->
                         </div>
                     </div>
+                    <div my-30px>
+                        <el-avatar shape="square" :size="164" :src="avatar"></el-avatar>
+                    </div>
+                    <div>
+                        <span block text-2xl>{{name}}</span>
+                        <slot name="skills"></slot>
+                        <!-- <span block>{{item.info}}</span> -->
+                    </div>
+                    <div h-80px mt-30px mb-10px mx-10px flex justify-center items-center border-solid border black-200>
+                        <slot name="intro"></slot>
+                        <!-- <span inline-block mx-10px>{{item.intro}}</span> -->
+                    </div>
                 </div>
-
             </div>
+
         </div>
+
     </div>
 
 </template>
-
-
-<style>
-/* .UserCard {
-    width: 1668px;
-    background: #000;
-}
-
-.card-list {
-    width: 1650px;
-    margin: 0px 14px 100px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
-.list {
-    margin: 20px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-} */
-
-/* .item {
-    margin: 0 20px;
-    width: 380px;
-    height: 470px;
-    text-align: center;
-    border-radius: 20px;
-    background-color: grey;
-
-} */
-
-/* .info-icon {
-    height: 30px;
-    width: 30px;
-    margin-left: 20px;
-    margin-top: 20px;
-} */
-
-/* .share-icon {
-    height: 20px;
-    width: 130px;
-    margin-right: 10px;
-    margin-top: 20px;
-} */
-
-/* .item-logo {
-    height: 40px;
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-} */
-/* a{
-    margin-right: 15px;
-} */
-/* .share {
-    display: inline-block;
-    width: 25px;
-    height: 25px;
-    background: url('https://api.iconify.design/carbon:share.svg') no-repeat center;
-    background-size: contain;
-    opacity: 0.5;
-}
-.bluebird{
-    display: inline-block;
-    width: 25px;
-    height: 25px;
-    background: url('./../imgs/bluebird.svg') no-repeat center;
-    background-size: contain;
-    opacity: 0.5;
-}
-.github{
-    display: inline-block;
-    width: 25px;
-    height: 25px;
-    background: url('./../imgs/github.svg') no-repeat center;
-    background-size: contain;
-    opacity: 0.5;
-}
-
-.item-img {
-    margin: 30px 0;
-}
-
-.intro {
-    display: inline-block;
-    margin: 0 10px;
-
-}
-
-.item-intro {
-    height: 80px;
-    margin: 30px 10px 10px;
-    border: solid 1px #333333;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-} */
-</style>
--- pilothouse-web/src/pages/demoTestPlayground/index.vue
+++ pilothouse-web/src/pages/demoTestPlayground/index.vue
@@ -1,8 +1,48 @@
 <template>
-    <div w-full flex>
-        <UserCard />
+    <div w-full flex justify-center items-center>
+        <UserCard v-for="(item,i) in cardList" :key="i" :avatar="item.avatar" :name="item.name" :bgcolor="item.bgcolor" :arr="item.arr">
+            <template v-slot:icon>
+                <img w-30px h-30px ml-20px mt-20px :src="item.icon" alt="" />
+            </template>
+            <template v-slot:skills>
+                <span block>{{item.skills}}</span>
+            </template>
+            <template v-slot:intro>
+                <span inline-block mx-10px>{{item.intro}}</span>
+            </template>
+        </UserCard>
+        
     </div>
 </template>
 <script setup lang="ts">
 import UserCard from '~/components/UserCard.vue';
+const cardList: any = reactive([
+    {
+        avatar: './../imgs/1.png',
+        name: 'Guillaume Chau',
+        skills: 'Vue / Histoire',
+        intro: 'Histoire Deep Dive',
+        icon: './../imgs/icon1.svg',
+        bgcolor: 'bg-gradient-to-b from-green-300 via-green-400 to-green-500',
+        arr:'12',
+    },
+    {
+        avatar: './../imgs/2.png',
+        name: 'Vojtech Miksu',
+        skills: 'Uber / Ladle',
+        intro: 'introducing Ladle:Develop and test your React stories faster',
+        icon: './../imgs/icon2.svg',
+        bgcolor: 'bg-gradient-to-b from-blue-300 via-blue-400 to-blue-500',
+        arr:'13',
+    },
+    {
+        avatar: './../imgs/3.png',
+        name: 'lan VanSchooten',
+        skills: 'Defined Networking / Storybook',
+        intro: 'Developing,Documenting,and Testing your Vite app with Stroybook',
+        icon: './../imgs/icon3.svg',
+        bgcolor: 'bg-gradient-to-b from-red-300 via-red-400 to-red-500',
+        arr:'123',
+    }
+])
 </script>
